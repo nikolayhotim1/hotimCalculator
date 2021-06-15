bbec5118c37ac6ecf54ff156e8ae3fdf8a45c2d1
nikolayhotim1 <hotim_nikolay@mail.ru>
2021-06-16 00:30:19 +0300

    refactoring #4

--- JS/main.js
+++ JS/main.js
@@ -31,7 +31,9 @@ function onOperationButtonClick(eventObject) {
     makeOperation(operation);
 }
 
-buttonPlus.addEventListener("click", onOperationButtonClick);
-buttonMinus.addEventListener("click", onOperationButtonClick);
-buttonMultiply.addEventListener("click", onOperationButtonClick);
-buttonDevide.addEventListener("click", onOperationButtonClick);
+let operationButtons = [buttonPlus, buttonMinus, buttonMultiply, buttonDevide];
+
+for (let i = 0; i < operationButtons.length; i++) {
+    let button = operationButtons[i];
+    button.addEventListener("click", onOperationButtonClick);
+}
